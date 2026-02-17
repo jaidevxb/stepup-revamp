769b327149a637a2b8735285fbf907d795cd13f7
Jaidev B <jaidevsboa@gmail.com>
2026-02-17 22:58:34 +0530

    update button

--- components/sections/FinalCTA.tsx
+++ components/sections/FinalCTA.tsx
@@ -29,7 +29,7 @@ export default function FinalCTA() {
               <ArrowRight className="ml-2 h-5 w-5" />
             </Button>
           </Link>
-          <Button size="lg" variant="outline" className="border-white text-white hover:bg-white/10" onClick={scrollToTracks}>
+          <Button size="lg" variant="outline" className="border-white text-white bg-transparent hover:bg-white/10 hover:text-white" onClick={scrollToTracks}>
             View All Tracks
           </Button>
         </div>
